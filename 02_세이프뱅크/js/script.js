583d68c736c8afa355ef203e5341f061a916c2c4
cogito30 <cogito30g@gmail.com>
2026-02-25 09:32:00 +0900

    feat: 세이프뱅크 slide

--- 02_세이프뱅크/js/script.js
+++ 02_세이프뱅크/js/script.js
@@ -6,5 +6,20 @@ $(function(){
     $("nav>ul>li").mouseout(function(){
         $(".sub, .menuBg").stop().slideUp();
     })
+
+    //slide
+    let i = 0;
+
+    function slide() {
+        if (i<2) {
+            i++;
+        } else {
+            i = 0;
+        }
+        $(".slide ul li").fadeOut();
+        $(".slide ul li").eq(i).fadeIn();
+    }
+
+    setInterval(slide, 2000);
 })
 
