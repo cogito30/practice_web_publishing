6d1b5d655dd36f5966d6606a586276aaacb30b03
cogito30 <cogito30g@gmail.com>
2026-02-25 09:20:29 +0900

    feat: 세이프뱅크 header

--- 02_세이프뱅크/js/script.js
+++ 02_세이프뱅크/js/script.js
@@ -1,4 +1,10 @@
 $(function(){
-    
+    //menu
+    $("nav>ul>li").mouseover(function(){
+        $(".sub, .menuBg").stop().slideDown();
+    })
+    $("nav>ul>li").mouseout(function(){
+        $(".sub, .menuBg").stop().slideUp();
+    })
 })
 
