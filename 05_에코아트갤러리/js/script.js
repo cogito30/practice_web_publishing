af7c9e3adae00f86ccb424b9a7337b08194e97ee
cogito30 <cogito30g@gmail.com>
2026-02-27 15:15:29 +0900

    feat: 05_에코아트갤러리 header

--- 05_에코아트갤러리/js/script.js
+++ 05_에코아트갤러리/js/script.js
@@ -1,3 +1,9 @@
 $(function() {
-    
+    //menu
+    $("nav ul li").mouseover(function (){
+        $(this).children(".sub").stop().fadeIn(200);
+    })
+    $("nav ul li").mouseleave(function (){
+        $(".sub").stop().fadeOut(200);
+    })
 })
