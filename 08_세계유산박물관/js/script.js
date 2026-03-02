cbe146cd7461673a83f63992e009baa3f5340cda
cogito30 <cogito30g@gmail.com>
2026-03-02 12:01:18 +0900

    08_세계유산박물관 header

--- 08_세계유산박물관/js/script.js
+++ 08_세계유산박물관/js/script.js
@@ -1,3 +1,9 @@
 $(function() {
+    $("nav>ul>li").mouseenter(function(){
+        $(this).children(".sub").stop().slideDown();
+    })
+    $("nav>ul>li").mouseleave(function(){
+        $(".sub").stop().slideUp();
+    })
 
 })
